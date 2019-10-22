1ae19c36ca1f8937f8533b1234ca4138ef525892
jroghair <jroghair@iastate.edu>
2019-10-22 15:26:25 -0500

    Finished pointwise, added backprop, started fleshing out the training

--- MobileNet_Model.py
+++ MobileNet_Model.py
@@ -1,6 +1,7 @@
 
 
 import numpy as np
+import tensorflow as tf
 
 def zero_padding(X, n):
     X_padded = np.pad(X,((0,0),(n,n),(n,n),(0,0)),'constant',constant_values=(0,0))
@@ -49,7 +50,7 @@ def conv_forward_layer(X, w, b, stride, padding):
                     #perform convolution
                     rs, re, cs, ce = (stride*row), (stride*row+f), (stride*col), (stride*col+f)
                     x_slice_prev = X_padded[i, rs:re, cs:ce, :] #apply filter across all channels
-                    print((x_slice_prev.shape, w.shape))
+                    #print((x_slice_prev.shape, w.shape))
                     Z[i, row, col, d] = perform_convolution(x_slice_prev, w[:,:,:,d], b[:,:,:,d]) 
                     #apply activation
                     Z[i, row, col, d] = relu(Z[i,row,col,d])
@@ -108,7 +109,7 @@ A_prev = np.random.randn(2,4,4,3) #(#images, #rows, #cols, #channels_prev)
 padding = 2
 stride = 2
 A, A_prev_cache = pooling_forward_layer(A_prev, stride, 3, method="avg")
-            
+               
 def DW_conv_layer(X, w, b, stride, padding):
     
     #Dimensions of input
@@ -135,12 +136,9 @@ def DW_conv_layer(X, w, b, stride, padding):
     for i in range(m):
         for d in range(n_depth_prev): #Each channel of the input
             #for each image, apply to each of its channels seperately its corresponding filter 
-            for row in range(n_row_prev):
-                for col in range(n_col_prev):
+            for row in range(n_row_out):
+                for col in range(n_col_out):
                     rs, re, cs, ce = (stride*row), (stride*row + f), (stride*col), (stride*col + f)
-                    #Not sure if this if statement is correct
-                    if re > n_row_prev  or ce > n_col_prev:
-                        continue
                         #print((i, rs,re, cs,ce, d))
                     x_slice =  X_padded[i, rs:re, cs:ce, d] #.reshape(f,f,1)
                         #print((x_slice.shape, w[:,:,d].shape))
@@ -157,6 +155,8 @@ padding = 1
 stride = 2
 
 Z = DW_conv_layer(A_prev, W_prev, b_prev, stride, padding)
+         
+
 
 def pointwise_conv_layer(X, w, b, stride, padding):
 
@@ -164,17 +164,19 @@ def pointwise_conv_layer(X, w, b, stride, padding):
     (m, n_row_prev, n_col_prev, n_depth_prev) = X.shape
     
     #Dimensions of kernel
-    (f, f, n_depth_prev, n_depth) = w.shape
+    (f, f, n_depth_prev) = w.shape
      
     #Dimensions of output
-    n_row_out = int((n_row_prev - f + 2*padding)/stride) + 1
-    n_col_out = int((n_col_prev - f + 2*padding)/stride) + 1    
+    #n_row_out = int((n_row_prev - f + 2*padding)/stride) + 1
+    #n_col_out = int((n_col_prev - f + 2*padding)/stride) + 1
+    n_row_out = n_row_prev
+    n_col_out = n_col_prev    
+    n_depth = n_depth_prev
     
     #Apply padding to input to retain dimensions
-    X_padded = zero_padding(X, padding)
-    print("X_padded shape: ", X_padded.shape)
+    #X_padded = zero_padding(X, padding)
+    #print("X_padded shape: ", X_padded.shape)
 
-    
     #Output Dimensions
     out = np.zeros((m, n_row_out, n_col_out, n_depth))
     print("out shape: ", out.shape)
@@ -182,17 +184,91 @@ def pointwise_conv_layer(X, w, b, stride, padding):
     #1x1 seperable convolution 
     for i in range(m):
         for d in range(n_depth): #N in the paper
-            for i in range(n_row_out):
-                for j in range(n_col_out):
+            for row in range(n_row_out):
+                for col in range(n_col_out):
                     rs, re, cs, ce = (stride*row), (stride*row + f), (stride*col), (stride*col +f)
-                    x_slice = X_padded[i, rs:re, cs:ce, d]
-                    out[i, row, col, d] = perform_convolution(x_slice, w[:,:,:,d], bias[:,:,:,d]) 
+                    x_slice = X[i, rs:re, cs:ce, :]
+                    out[i, row, col, d] = perform_convolution(x_slice, w[:,:,:], b[:,:,d]) 
+    
+    return out
 
+
+#Test Pointwise Layer
+np.random.seed(1)
+A_prev = np.random.randn(1,56,56,64) #(#images, #rows, #cols, #channels_prev)
+W_prev = np.random.randn(1,1,64) #(#rows, #cols, #channels_prev)
+b_prev = np.random.randn(1,1,64) #(1,1,1, #channels_out)
+padding = 0
+stride = 1
+
+Z = pointwise_conv_layer(A_prev, W_prev, b_prev, stride, padding)  
+
+def conv_back_layer():
+    
+    
+def pool_back_layer(dA, cache, method="max"):
+    X_prev, stride, ps = cache
+    
+    m, n_row_prev, n_col_prev, n_depth_prev = X_prev.shape
+    m, n_row, n_col, n_depth = dA.shape
+    
+    #output dimensions, going backward in the network so its the same dimensions at X_prev
+    dA_prev = np.zeros_like(X_prev)
+    
+    for i in range(m):
+        for row in range(n_row):
+            for col in range(n_col):
+                for d in range(n_depth):
+                    rs, re, cs, ce = (stride*row), (stride*row + f), (stride*col), (stride*col + f)
+                    if method == "max":
+                        x_prev_slice = X_prev[i, rs:re, cs:ce, d]
+                        largest = (x_prev_slice == np.max(x_prev_slice))
+                        dA_prev[i, rs:re, cs:ce, d] += largest*dA[i, rs, cs, d]
+                    elif method == "avg":
+                        da = dA[i, rs, cs, d]
+                        avg = np.ones((ps,ps))*(da/(ps*ps))
+                        dA_prev[i, rs:re, cs:ce, d] += avg
+                        
+    return dA_prev
+                        
+    
+    
+    
+    
     
 
+def RMSprop():
+    
+    
+def initalize_weights(dim):
+    '''
+    Randomly intalizes weights for input layer of CNN
+    0.01 constant is used to ensure the weights are initally really small so that
+    our Z values (where Z= Wx+b) arent initally large and GD learning isnt initally slow
+    '''
+    
+    return np.random.randn(dim)*0.01
 
+def training(images, n_classes, batch_size, n_epochs):
+    
+    #initalize weights
+    
+    
+    #train
+    for epoch in range(n_epochs):
+        
+        
+        for batch in range(batch_size):
+            
     
 
+    #export model with pickle
+    with open(path, 'wb') as file:
+        pickle.dumb(params, file)
+        
+        
+    return cost
+    
 
 
             
